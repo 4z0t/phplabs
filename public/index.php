f1b0cdfdb375fdebcfc99822bfd14caa608dc89e
Alexander <84857900+4z0t@users.noreply.github.com>
2023-02-22 12:43:00 +0300

    wip

--- public/index.php
+++ public/index.php
@@ -1,5 +1,25 @@
 <?php
 
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Validation;
+
+class User
+{
+    #[Assert\NotBlank]
+    public $name;
+    #[Assert\NotBlank]
+    public $mail;
+
+    public function __construct($name, $mail)
+    {
+        $this->name = $name;
+        $this->mail = $mail;
+    }
+}
+
 define('LARAVEL_START', microtime(true));
 
 
@@ -10,3 +30,25 @@ echo "ABOBA";
 
 
 readfile("index.html");
+
+$user1 = new User("Sasha", "Mail");
+$user2 = new User("", "");
+
+
+
+
+$validator = Validation::createValidator();
+$errors1 = $validator->validate($user1);
+$errors2 = $validator->validate($user2);
+
+if (count($errors1)>0)
+{
+    $errstr = (string) $errors1;
+    echo "1:$errstr";
+}
+
+if (count($errors2)>0)
+{
+    $errstr = (string) $errors2;
+    echo "2:$errstr";
+}
