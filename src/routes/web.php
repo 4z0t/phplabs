576da4ae7be9a2ce96c38deacea7563773011e4c
4z0t <gastec139@gmail.com>
2023-04-12 19:56:20 +0300

    remove test code

--- src/routes/web.php
+++ src/routes/web.php
@@ -18,46 +18,5 @@ use App\Classes\Comment;
 */
 
 Route::get('/', function () {
-    $user1 = new User("Sasha", "Mail");
-    $user2 = new User("", "");
-
-    $user1->creationTime->modify('-1 year');
-
-
-    $validator = new UserValidator();
-    $errors1 = $validator->validate($user1);
-    $errors2 = $validator->validate($user2);
-
-
-    echo "user1" . "<br/>";
-    foreach ($errors1 as $field => $constrainsErrors) {
-        if (count($constrainsErrors) > 0) {
-            echo "$field: $constrainsErrors<br/>";
-        }
-    }
-    echo "user2" . "<br/>";
-    foreach ($errors2 as $field => $constrainsErrors) {
-        if (count($constrainsErrors) > 0) {
-            echo "$field: $constrainsErrors<br/>";
-        }
-    }
-
-
-
-    $comments = [
-        new Comment($user1, "Hello world!"),
-        new Comment($user1, "AAAAAAA"),
-        new Comment($user2, "BBBBBBBBBB"),
-        new Comment($user2, ",mfnsdkjfhdskfhksdfj"),
-    ];
-
-    $checkDate = new DateTime('now');
-    $checkDate->modify('-1 month');
-
-
-    foreach ($comments as $comment) {
-        if ($comment->user->creationTime > $checkDate) {
-            echo $comment->text . "<br/>";
-        }
-    }
+    
 });
