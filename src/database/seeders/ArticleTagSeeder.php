9e09eb7f290f7805c55301870a6705bdd907b1f6
4z0t <gastec139@gmail.com>
2023-04-12 21:38:54 +0300

    add seeder for article-tag relation

---
+++ src/database/seeders/ArticleTagSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+
+use App\Models\Article;
+use App\Models\Tag;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ArticleTagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $articles = Article::all();
+        $tags = Tag::all();
+
+        $articles->each(function ($article) use ($tags) {
+            $article->tags()->attach(
+                $tags->random(10)->pluck('id')->toArray()
+            );
+        });
+    }
+}
