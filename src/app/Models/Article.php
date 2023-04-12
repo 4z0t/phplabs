10a842d3c2e1828002af56d681be0648dbaf27a0
4z0t <gastec139@gmail.com>
2023-04-12 19:04:19 +0300

    add relations between tables

--- src/app/Models/Article.php
+++ src/app/Models/Article.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Article extends Model
 {
     use HasFactory;
+
+    /**
+     * The tags of articles
+     */
+    public function tags(): BelongsToMany
+    {
+        return  $this->belongsToMany(Tag::class);
+    }
 }
