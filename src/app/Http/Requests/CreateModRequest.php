ac29738de92a53e010d70411eb314bbdde292106
4z0t <gastec139@gmail.com>
2023-05-29 22:21:38 +0300

    implement CreateModRequest

--- src/app/Http/Requests/CreateModRequest.php
+++ src/app/Http/Requests/CreateModRequest.php
@@ -7,14 +7,6 @@ use Illuminate\Foundation\Http\FormRequest;
 class CreateModRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
@@ -22,7 +14,13 @@ class CreateModRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "name" => "required|max:255|unique:mods,name",
+            "description" => "required",
+            "author_id" => "required|exists:author,id",
+            "major_version" => "required", //something for integer required...
+            "minor_version" => "required", //something for integer required...
+            "patch_version" => "required", //something for integer required...
+            "link" => "required",
         ];
     }
 }
