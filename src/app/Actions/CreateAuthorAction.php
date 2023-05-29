f00b7e364f8258b318191fc657523d5936ab0f32
4z0t <gastec139@gmail.com>
2023-05-29 22:04:18 +0300

    implement CreateAuthorAction

--- src/app/Actions/CreateAuthorAction.php
+++ src/app/Actions/CreateAuthorAction.php
@@ -2,13 +2,13 @@
 
 namespace App\Actions;
 
+use App\Http\Requests\CreateAuthorRequest;
 use App\Models\Author;
 
 class CreateAuthorAction
 {
-
-    public function handle($request) : Author
+    public function handle(CreateAuthorRequest $request) : Author
     {
-
+        return Author::create($request->validated());
     }
 }
