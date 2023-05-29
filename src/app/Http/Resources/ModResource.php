f5ca4089092b6de7eee8e6199072fad434cde079
4z0t <gastec139@gmail.com>
2023-05-29 22:41:52 +0300

    implemnt Resources

--- src/app/Http/Resources/ModResource.php
+++ src/app/Http/Resources/ModResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Models\Author;
 
 class ModResource extends JsonResource
 {
@@ -14,6 +15,17 @@ class ModResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "description" => $this->description,
+            "author" => AuthorResource::collection(Author::find($this->author_id)),
+            "major_version" => $this->major_version,
+            "minor_version" => $this->minor_version,
+            "patch_version" => $this->patch_version,
+            "link" => $this->link,
+            "created_at" => $this->created_at,
+            "updated_at" => $this->updated_at,
+        ];
     }
 }
--- src/app/Models/Author.php
+++ src/app/Models/Author.php
@@ -2,8 +2,11 @@
 
 namespace App\Models;
 
+use App\Models\Mod;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasRelationships;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
  * Class Author
@@ -14,4 +17,9 @@ use Illuminate\Database\Eloquent\Model;
 class Author extends Model
 {
     use HasFactory;
+
+    public function mods() : HasMany
+    {
+       return $this->hasMany(Mod::class);
+    }
 }
