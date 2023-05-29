5ea4ba881a3eee9f85f965a8d324c6aee8f33c31
4z0t <gastec139@gmail.com>
2023-05-29 22:41:41 +0300

    implement Update Requests

--- src/app/Http/Requests/UpdateAuthorRequest.php
+++ src/app/Http/Requests/UpdateAuthorRequest.php
@@ -4,25 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateAuthorRequest extends FormRequest
+class UpdateAuthorRequest extends CreateAuthorRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
-     */
-    public function rules(): array
-    {
-        return [
-            //
-        ];
-    }
 }
