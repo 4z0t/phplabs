c7b0ad2fc6ea7df3340225251fbbebf093cd5acf
4z0t <gastec139@gmail.com>
2023-05-29 22:22:50 +0300

    implement DeleteModRequest and DeleteAuthorRequest

--- src/app/Http/Requests/DeleteModRequest.php
+++ src/app/Http/Requests/DeleteModRequest.php
@@ -7,14 +7,6 @@ use Illuminate\Foundation\Http\FormRequest;
 class DeleteModRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
@@ -22,7 +14,7 @@ class DeleteModRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "id" => "required"
         ];
     }
 }
